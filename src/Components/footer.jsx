1d171b0de416eccd6b0a525bd15a7b20b8551d6a
gaurav singh <thakurgaurav273@gmail.com>
2023-10-08 03:37:42 +0530

    some changes

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="column">
-        <h3>Company</h3>
+        <h3 className='footer-h3'>Company</h3>
         <ul>
           <li><a href="#">About Us</a></li>
           <li><a href="#">Careers</a></li>
@@ -14,20 +14,20 @@ const Footer = () => {
         </ul>
       </div>
       <div className="column">
-        <h3>View Website In</h3>
+        <h3 className='footer-h3'>View Website In</h3>
         <ul>
           <li><a href="#"><i className="fa-solid fa-check check"></i> English</a></li>
         </ul>
       </div>
       <div className="column">
-        <h3>Need Help ?</h3>
+        <h3 className='footer-h3'>Need Help ?</h3>
         <ul>
           <li><a href="#">Visit Help Center</a></li>
           <li><a href="#">Share Feedback</a></li>
         </ul>
       </div>
       <div className="column">
-        <h3>Connect with Us</h3>
+        <h3 className='footer-h3'>Connect with Us</h3>
         <ul>
           <li><a href="#"><i className="fa-brands fa-facebook-f social"></i> </a> <a><i className="fa-brands fa-twitter social"></i></a></li>
           <li><a href="/"><img alt="pic_is_there" className="googleplay" src="https://img1.hotstarext.com/image/upload/f_auto,q_90,w_256/v1661346101/google-playstore"  srcSet="" /></a></li>
